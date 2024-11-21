079d7ad866dfdd349cd14ae541bd5eca6860371a
edbertosantos <edbertosantos.dev@gmail.com>
2024-11-21 03:06:00 -0300

    Todo Finalizado

--- src/assets/components/TodoItem.jsx
+++ src/assets/components/TodoItem.jsx
@@ -1,20 +1,29 @@
 // src/components/TodoItem.jsx
 import "../css/TodoItem.css";
+import { useState } from "react";
 
-const TodoItem = ({ todo, toggleComplete, deleteTodo }) => (
-  <li className="TodoItem">
-    <div className="TodoItemDIV">
-      <span
-        style={{ textDecoration: todo.completed ? "line-through" : "none" }}
-      >
-        {todo.text}
-      </span>
-      <button onClick={toggleComplete} className="">
-        {todo.completed ? "Desfazer" : "Concluir"}
-      </button>
-      <button onClick={deleteTodo}>Excluir</button>
-    </div>
-  </li>
-);
+const TodoItem = ({ todo, toggleComplete, deleteTodo }) => {
+  const [borderColor, setBorderColor] = useState("rgb(255, 0, 0)");
+
+  const handleChangeBorderColor = () => {
+    const newColor =
+      borderColor === "rgb(255, 0, 0)" ? "rgb(0, 255, 0)" : "rgb(255, 0, 0)";
+    setBorderColor(newColor);
+  };
+
+  return (
+    <li className="TodoItem" style={{ borderColor: borderColor }}>
+      <div className="TodoItemCard">
+        <span>{todo.text}</span>
+        <div>
+          <button onClick={() => {toggleComplete(); handleChangeBorderColor()}} className="Troca">
+            {todo.completed ? "Desfazer" : "Concluir"}
+          </button>
+          <button onClick={deleteTodo}>Excluir</button>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default TodoItem;
